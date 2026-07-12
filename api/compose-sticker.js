d8904f9d91c87441d8a5ee4f0da87d312f9b518a
switchcoververse-commits <switchcoververse@gmail.com>
2026-07-12 16:17:32 +0000

    Fix: change blend to src-over

--- api/compose-sticker.js
+++ api/compose-sticker.js
@@ -112,7 +112,7 @@ export default async function handler(req, res) {
 
     const result = await sharp(textureBuffer)
       .resize(mapping.w, mapping.h, { fit: 'fill' })
-      .composite([{ input: maskPath, blend: 'dest-in' }])
+      .composite([{ input: maskPath, blend: 'src-over' }])
       .png()
       .toBuffer();
 
